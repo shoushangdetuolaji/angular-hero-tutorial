859e61db141a943fdc0cffd29992a7a8cefcee7f
shoushangdetuolaji <tklijunhao@qq.com>
2021-12-23 11:50:08 +0900

    feat: add rxjs创建类操作符

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import {
 } from '@angular/core';
 
 import {TransferItem} from "./demos/components/transfer/types";
-import { fromEvent } from 'rxjs';
-import {scan, throttleTime} from "rxjs/operators";
+import {empty, from, fromEvent, Observable, of, map, interval, timer, iif} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,22 +18,52 @@ export class AppComponent {
   constructor(
   ) {}
   ngOnInit(): void {
-    // let count = 0;
-    // const rate = 1000;
-    // let lastClick = Date.now() - rate;
-    // document.addEventListener('click', () => {
-    //   if (Date.now() - lastClick >= rate) {
-    //     console.log(`Click ${++count} times`);
-    //     lastClick = Date.now();
-    //   }
+    // const source = of({name: 'Brain'}, [1, 2, 3], function hello() {
+    //   return 'Hello';
     // });
-    fromEvent(document, 'click')
-      .pipe(
-        throttleTime(1000),
-        scan(count => count + 1, 0)
-      )
-      .subscribe(count => console.log(`Click ${count} times`));
+    // const arraySource = from([1, 2, 3, 4, 5]);
+    // const arraySource = from(new Promise(resolve => resolve('hello world')));
+    // const arraySource = from(Promise.reject(new Error('err promise')));
 
 
+    // const subscribe = arraySource.subscribe(
+    //   val => console.log(val),
+    //   error => console.error(error)
+    // );
+
+    // const map = new Map([
+    //   [1, 'hi']
+    // ]);
+    // map.set(2, 'Bye');
+    // map.set(3, 'rxjs');
+    // const mapSource = from(map);
+    // console.log(map)
+    // mapSource.subscribe(res => console.log(res));
+
+    // const result = empty();
+    // result.subscribe(
+    //   res => console.log(res),
+    //   error => {},
+    //   () => console.log('ok')
+    // )
+
+    // const source = fromEvent(document, 'click');
+    // const example = source.pipe(map(event => `Event time: ${event.timeStamp}`));
+    // example.subscribe(val => console.log(val))
+
+    // const source = timer(1000);
+    // const subscribe = source.subscribe(val => console.log(val));
+
+    const random = Math.random();
+    console.log('random', random);
+    const firstOrSecond = iif(
+      () => random > 0.5,
+      of('first'),
+      of('second'),
+    );
+    firstOrSecond.subscribe(res => {
+      console.log('res', res);
+    });
   }
+
 }
